297e518f09c2a4f2dc6f67f4d854732b273af6b6
gloriayordanova <raevagloria@gmail.com>
2025-08-27 13:53:41 +0100

    Final Version

--- cypress/support/step_definitions/Homepage_Steps.js
+++ cypress/support/step_definitions/Homepage_Steps.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 import { Given, When, Before } from "@badeball/cypress-cucumber-preprocessor";
-import Base_PO from "../page-objects/Base_PO";
+// import Base_PO from "../page-objects/Base_PO";
+import Homepage_PO from "../page-objects/Homepage_PO";
 
 //const url = "https://www.webdriveruniversity.com/";
 
-const basePage = new Base_PO();
+//const basePage = new Base_PO();
+const homePage = new Homepage_PO();
 
 Before(() => {
     cy.log("Executing commands inside Homepage Steps");
@@ -12,16 +14,19 @@ Before(() => {
 
 Given(`I navigate to the webdriveruniversity homepage`, () => {
     //cy.visit(url);
-    basePage.navigate("");
-    basePage.getPageTitle();
+    // basePage.navigate("");
+    homePage.navigate("");
+    // basePage.getPageTitle();
 })
 
 When (`I click on the contact us button`, () => {
     //cy.get("#contact-us").invoke("removeAttr", "target").click();
-    cy.clickAndOpenLink_InSameTab("#contact-us");
+    //cy.clickAndOpenLink_InSameTab("#contact-us");
+    homePage.clickOn_ContactUs_Button();
 })
 
 When (`I click on the login portal button`, () => {
     //cy.get("#login-portal").invoke("removeAttr", "target").click();
-    cy.clickAndOpenLink_InSameTab("#login-portal");
+    //cy.clickAndOpenLink_InSameTab("#login-portal");
+    homePage.clickOn_Login_Button();
 })
